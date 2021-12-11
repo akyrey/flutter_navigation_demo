927e182d1e6354f1d69e92eaed8ece76dbf20013
Dario Vogogna <stein.dario@gmail.com>
2021-12-11 10:17:12 +0100

    fix: migrate buttons to new widgets

--- lib/screen1.dart
+++ lib/screen1.dart
@@ -9,8 +9,7 @@ class Screen1 extends StatelessWidget {
         title: Text('Screen 1'),
       ),
       body: Center(
-        child: RaisedButton(
-          color: Colors.red,
+        child: ElevatedButton(
           child: Text('Go Forwards To Screen 2'),
           onPressed: () {},
         ),
